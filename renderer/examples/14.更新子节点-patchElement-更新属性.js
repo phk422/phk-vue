9dd66be2ace5b4a40715980ba6c17db3ed578312
phk422 <penghongkun422@gmail.com>
2024-09-24 20:28:43 +0800

    chore(renderer): 完善案例

--- renderer/examples/14.更新子节点-patchElement-更新属性.js
+++ renderer/examples/14.更新子节点-patchElement-更新属性.js
@@ -9,20 +9,19 @@ const vnode = shallowRef({
   props: {
     id: 'foo',
     class: 'bar',
+    onClick() {
+      console.log('update-props')
+      vnode.value = {
+        type: 'button',
+        children: '新按钮',
+        props: {
+          id: 'new-foo',
+        },
+      }
+    },
   },
 })
 
 effect(() => {
   render(vnode.value, document.getElementById('app'))
 })
-
-setTimeout(() => {
-  // 两秒后更新虚拟
-  vnode.value = {
-    type: 'button',
-    children: '新按钮',
-    props: {
-      id: 'new-foo',
-    },
-  }
-}, 2000)
